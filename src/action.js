919ea7a1e80dcd9a9d5d434522ab85e078bbd379
bingo <binhdu@foxmail.com>
2020-01-13 19:34:58 +0800

    emit action

--- src/action.js
+++ src/action.js
@@ -2,6 +2,6 @@ let fs = require('fs')
 let path = require('path')
 console.log(__dirname)
 fs.writeFile(path.resolve(__dirname, './user.json'), '[123]', 'utf8', (err) => {
-  if (err) throw err;
+  if (err) throw err; 
   console.log('done');
 })
